b5c69a9c2e03915285f1b889fe2e3385ace52e17
AntonioF842 <antoemman@hotmail.com>
2025-05-16 15:55:32 -0600

    [ECOM-15]Creación de header y footer

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,30 +1,24 @@
+import { TuiRoot } from '@taiga-ui/core';
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-
 // Componentes personalizados
 import { HeaderComponent } from './shared/header/header.component';
 import { FooterComponent } from './shared/footer/footer.component';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
-    CommonModule,
-    RouterOutlet,
-    HeaderComponent,
+    RouterOutlet, 
+    TuiRoot,
+    CommonModule, 
+    HeaderComponent, 
     FooterComponent
   ],
-  template: `
-    <div class="flex flex-col min-h-screen">
-      <app-header></app-header>
-      <main class="flex-grow">
-        <router-outlet></router-outlet>
-      </main>
-      <app-footer></app-footer>
-    </div>
-  `,
-  styleUrls: ['./app.component.css']
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'EComerce-UG';
--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -13,7 +13,6 @@ export const appConfig: ApplicationConfig = {
     provideRouter(routes), 
     provideClientHydration(withEventReplay()), 
     provideEventPlugins(),
-        provideEventPlugins(),
         provideEventPlugins()
     ]
 };
