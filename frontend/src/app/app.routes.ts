e1ba849f8f4982513c6b0a9994aff823e2ecd34f
0SU2 <or_zv18v@outlook.com>
2025-05-18 00:52:39 -0600

    [ECOM-6]: Creacion de la vista para los productos, arreglos en imagenes y información

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,7 +1,15 @@
 import { Routes } from '@angular/router';
 import { ShopComponent } from './pages/shop/shop.component';
+import { ProductComponent } from './pages/product/product.component';
 
 export const routes: Routes = [
-  { path: 'shop', component: ShopComponent },
+  { 
+    path: 'product-view/:id', 
+    component: ProductComponent,
+  },
+  { 
+    path: 'shop', 
+    component: ShopComponent, 
+  },
   { path: '', redirectTo: '/shop', pathMatch: 'full' }
 ];
---
+++ frontend/src/interfaces.ts
@@ -0,0 +1,17 @@
+export interface ProductList {
+  id: number,
+  name: string,
+  price: number,
+  quantityAvailable: number,
+  discountPrice: number | null,
+  image: string,
+  category: string,
+  isNew: boolean,
+  isSale: boolean,
+  colorAvailable: string[],
+  rating: number,
+  quantityReviews: number,
+  productContext: string,
+  productDescription: string,
+  imagesRoute: string
+}
