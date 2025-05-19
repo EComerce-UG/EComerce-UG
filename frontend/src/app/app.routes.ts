664c41e4c9ac3b40621b82e188cf92499cf270e6
0SU2 <or_zv18v@outlook.com>
2025-05-18 19:47:28 -0600

    [ECOM-26]: Funcionalidad para cuando el usuario no haya iniciado sesion aun y quiera agregar productos al carrito

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,11 +1,18 @@
 import { Routes } from '@angular/router';
 import { ShopComponent } from './pages/shop/shop.component';
 import { ProductComponent } from './pages/product/product.component';
+import { CartComponent } from './pages/cart/cart.component';
+import { AuthguardService } from './auth/authguard.service';
 
 export const routes: Routes = [
   { 
     path: 'product-view/:id', 
     component: ProductComponent,
+  },
+  { 
+    path: 'cart', 
+    component: CartComponent, 
+    canActivate: [AuthguardService]
   },
   { 
     path: 'shop', 
