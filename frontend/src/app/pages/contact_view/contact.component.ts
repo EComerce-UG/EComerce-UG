407a36a3762f27a632beb04d9514c7b7e42d4360
Andy Bravo <andy_zequinelli@hotmail.com>
2025-05-20 09:02:47 -0600

    [ECOM-4]: Vista para contact

---
+++ frontend/src/app/pages/contact_view/contact.component.ts
@@ -0,0 +1,41 @@
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-contact',
+  templateUrl: './contact.component.html',
+  styleUrls: ['./contact.component.css']
+})
+export class ContactComponent {
+  contactForm: FormGroup;
+  submitted = false;
+  successMessage = '';
+
+  constructor(private fb: FormBuilder) {
+    this.contactForm = this.fb.group({
+      name: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      subject: ['', Validators.required],
+      message: ['', Validators.required]
+    });
+  }
+
+  get f() {
+    return this.contactForm.controls;
+  }
+
+  onSubmit() {
+    this.submitted = true;
+
+    if (this.contactForm.invalid) {
+      return;
+    }
+
+    // Aquí puedes manejar el envío, por ejemplo, enviar datos a un servidor
+    console.log('Formulario enviado:', this.contactForm.value);
+
+    this.successMessage = '¡Gracias por contactarnos! Te responderemos pronto.';
+    this.contactForm.reset();
+    this.submitted = false;
+  }
+}
