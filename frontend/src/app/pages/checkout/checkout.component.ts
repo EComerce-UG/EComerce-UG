99ead6da5f238dbb9fbebc9f91a10eecb4b36cff
0SU2 <or_zv18v@outlook.com>
2025-06-06 06:00:18 -0600

    FIXED: Ultimo cambio de la noche x3

--- frontend/src/app/pages/checkout/checkout.component.ts
+++ frontend/src/app/pages/checkout/checkout.component.ts
@@ -92,15 +92,6 @@ export class CheckoutComponent implements OnInit {
 
   ngOnInit(): void {
     this.loadCartData();
-    
-    // Verificar si el carrito está vacío
-    // if (this.cartItemsUser.length === 0) {
-    //   this.alerts.open('Your cart is empty. Please add items before checkout.', {
-    //     label: 'Empty Cart',
-    //     appearance: 'warning'
-    //   }).subscribe();
-    //   this.router.navigate(['/shop']);
-    // }
     this.userService.userCartTotalCost.subscribe((value) => {
       this.subtotalUser = value
     })
